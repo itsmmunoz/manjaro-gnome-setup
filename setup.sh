eec4d4481ae60393ddc4b53c77b9a8af025d25bb
Maycol  Muñoz <94150754+maycolmunoz@users.noreply.github.com>
2026-01-10 09:07:08 -0500

    add script

---
+++ setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+info() { 
+  echo -e "\e[1;34m[INFO]\e[0m $1" 
+}
+
+info "Open GNOME extensions"
+xdg-open https://extensions.gnome.org/extension/517/caffeine/
+xdg-open https://extensions.gnome.org/extension/779/clipboard-indicator/
+xdg-open https://extensions.gnome.org/extension/4167/custom-hot-corners-extended/
+xdg-open https://extensions.gnome.org/extension/3193/blur-my-shell/
+
+
+info "GNOME settings"
+gnome-extensions enable forge@jmmaranan.com
+
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
+
+gsettings set org.gnome.desktop.wm.preferences button-layout ':close'
+
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash true
+
+gsettings set org.gnome.shell.extensions.dash-to-dock show-show-apps-button false
+
+
+info "Configure pamac (AUR + Flatpak + keep builds)"
+sudo sed -i \
+  -e 's/^#EnableAUR/EnableAUR/' \
+  -e 's/^#CheckAURUpdates/CheckAURUpdates/' \
+  -e 's/^#EnableFlatpak/EnableFlatpak/' \
+  -e 's/^#CheckFlatpakUpdates/CheckFlatpakUpdates/' \
+  -e 's/^#KeepBuiltPkgs/KeepBuiltPkgs/' \
+  /etc/pamac.conf
+
+
+info "Remove default apps"
+PKGS=(
+  gnome-calendar gnome-contacts gnome-maps gnome-weather gnome-clocks
+  gnome-tour endeavour gnome-chess gnome-mines iagno malcontent
+  quadrapassel htop micro
+)
+
+sudo pamac remove --no-confirm $(pacman -Qq "${PKGS[@]}" 2>/dev/null) || true
+
+
+
+
